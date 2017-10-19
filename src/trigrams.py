16497e72135735ebec4ecaf38cfb09c96a99453f
jensjoh01 <johnnyj2439@gmail.com>
2017-10-19 14:52:09 -0700

    able to import file and populate dictionary with key value pairs

--- src/trigrams.py
+++ src/trigrams.py
@@ -1,11 +1,32 @@
+"""."""
 
+
+import io
 # read the file
-string = get the file
+f = io.open('test.txt', encoding='utf-8')
+string = f.read()
+f.close()
 
 # create an iterable list of all the words
 list_of_words = string.split()
 
-# iterate through each pair in the list
-for (var i = 0; i < list_of_words.length; i++) {
+# create a dictionary
+trigrams_dict = {}
 
-}
+
+# iterate through each pair in the list
+def main():
+    """."""
+    for i in range(len(list_of_words) - 2):
+        trigrams_dict_keys = ' '.join(list_of_words[i:i + 2])
+        try:
+            if len(trigrams_dict[trigrams_dict_keys]) > 0:
+                trigrams_dict[trigrams_dict_keys].append(list_of_words[i + 2])
+        except KeyError:
+            trigrams_dict[trigrams_dict_keys] = [list_of_words[i + 2]]
+
+
+main()
+
+
+print(trigrams_dict)
