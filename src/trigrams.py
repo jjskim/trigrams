989f934f37d98ddc26b9f7e9a54d54aef5e86dca
Joseph Kim <joseph.kim.kr@gmail.com>
2017-10-19 15:29:20 -0700

    finished working version -- need to clean up

--- src/trigrams.py
+++ src/trigrams.py
@@ -1,12 +1,5 @@
 """."""
 
-
-import io
-# read the file
-f = io.open('test.txt', encoding='utf-8')
-string = f.read()
-f.close()
-
 # create an iterable list of all the words
 list_of_words = string.split()
 
@@ -15,8 +8,14 @@ trigrams_dict = {}
 
 
 # iterate through each pair in the list
-def main():
+def main(file_path, num_words):
     """."""
+    import io
+    # read the file
+    f = io.open('test.txt', encoding='utf-8')
+    string = f.read()
+    f.close()
+
     for i in range(len(list_of_words) - 2):
         trigrams_dict_keys = ' '.join(list_of_words[i:i + 2])
         try:
@@ -25,8 +24,17 @@ def main():
         except KeyError:
             trigrams_dict[trigrams_dict_keys] = [list_of_words[i + 2]]
 
+    import random
+    new_key = random.choice(list(trigrams_dict.keys()))
+    new_text = new_key
+    while (new_key in list(trigrams_dict.keys())):
+        new_text = new_text + " " + random.choice(trigrams_dict[new_key])
+        new_key = " ".join(new_text.split()[-2:])
+
+    print(new_text)
+
 
 main()
 
 
-print(trigrams_dict)
+#print(trigrams_dict)
