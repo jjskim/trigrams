f3c945f85eba3367a3c66467959ed8e5b32b86a5
Joseph Kim <joseph.kim.kr@gmail.com>
2017-10-19 14:08:58 -0700

    initial setup of files

---
+++ src/trigrams.py
@@ -0,0 +1,11 @@
+
+# read the file
+string = get the file
+
+# create an iterable list of all the words
+list_of_words = string.split()
+
+# iterate through each pair in the list
+for (var i = 0; i < list_of_words.length; i++) {
+
+}
